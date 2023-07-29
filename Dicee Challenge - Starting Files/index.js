7267c8a52c85d00f4c9053aae0318a331521da42
ShibuJoseph18 <shibujoseph1815@gmail.com>
2023-07-29 19:19:18 +0530

    new

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+var randomDiceNumber1 = "images/" + "dice" + randomNumber1 + ".png";
+var randomDiceNumber2 = "images/" + "dice" + randomNumber2 + ".png";
+
+var img1 = document.querySelector(".img1").setAttribute("src",randomDiceNumber1);
+var img2 = document.querySelector(".img2").setAttribute("src",randomDiceNumber2);
+
+if(randomNumber1 > randomNumber2){
+    var heading1 = document.querySelector("h1").innerHTML = "Player 1 Wins";
+}
+else if(randomNumber1 === randomNumber2) {
+    var heading2 = document.querySelector("h1").innerHTML = "Draw";
+}
+else{
+    var heading3 = document.querySelector("h1").innerHTML = "Player 1 Lose";
+}
+
